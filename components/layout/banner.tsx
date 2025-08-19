769a5b1a8cb32770de4f3a19faaec28f12cbf61c
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-08-19 11:09:51 +0200

    fix

--- components/layout/banner.tsx
+++ components/layout/banner.tsx
@@ -1,18 +1,17 @@
 export function Banner() {
   return (
-    <div className="bg-gray-200 p-2 text-center text-black">
-      <p className="text-balance">
-        Check out{" "}
-        <a
-          href="https://chat.together.ai"
-          className="font-semibold underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Together Chat
-        </a>{" "}
-        to use DeepSeek R1 for free
-      </p>
-    </div>
+    <div className="bg-gray-200 p-2 text-center text-black relative">
+    <p className="text-balance">
+      Check out{" "}
+      <a
+        href="https://chat.together.ai"
+        className="font-semibold underline"
+        target="_blank"
+      >
+        Together Chat
+      </a>{" "}
+      to use DeepSeek R1 for free
+    </p>
+  </div>
   );
 }
